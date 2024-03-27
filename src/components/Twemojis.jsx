6bdf80dcd9265975e8fabb33d04c00674d534634
NiaAxern <gdmixerno@gmail.com>
2024-03-27 21:33:57 +0200

    Added website files

---
+++ src/components/Twemojis.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react';
+
+const Twemoji = ({ emoji,height,width }) => {
+    const [preEmoji, setPremoji] = useState(emoji);
+    useEffect(() => {
+        const emj = window.twemoji.parse(emoji);
+        setPremoji(emj)
+        console.log(emj)  
+        
+    });
+    
+    return(
+        
+        <span style={{width, height}} dangerouslySetInnerHTML={{__html: preEmoji}} className='Twemoji inline-block'></span>
+        
+    );
+    
+}
+
+export default Twemoji;
